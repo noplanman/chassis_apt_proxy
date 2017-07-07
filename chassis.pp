88c188b049418de4c31106781c24283cc5cc3766
Armando Lüscher <armando@noplanman.ch>
2017-07-07 04:37:17 +0200

    Make sure this extension gets installed before any others.

--- chassis.pp
+++ chassis.pp
@@ -1,9 +1,13 @@
 $apt_proxy_config = sz_load_config()
 
 if $apt_proxy_config[apt_proxy] {
+	stage {"preinstall_apt_proxy":
+		before => Stage['main'],
+	}
 	class {"apt-proxy":
 		file  => $apt_proxy_config[apt_proxy][file],
 		http  => $apt_proxy_config[apt_proxy][http],
 		https => $apt_proxy_config[apt_proxy][https],
+		stage => preinstall_apt_proxy,
 	}
 }
